8f7b96515addbb93b75ac5135bf143662605ac89
Ярослав Семченко <ukrkyi@gmail.com>
2018-11-23 20:09:58 +0200

    Fixed photo information read order
    Signed-off-by: Ярослав Семченко <ukrkyi@gmail.com>

--- test.py
+++ test.py
@@ -83,9 +83,9 @@ for i in range(0, cnt):
 num = int(out_file.readline())
 for i in range(0, num):
     f, l, t, s = map(int, out_file.readline().split())
-    sat[s].add_photo((t, f, l))
+    sat[s].add_photo((f, l, t))
     for collection in col:
-        collection.add_photo((t, f, l))
+        collection.add_photo((f, l, t))
 
 for satellite in sat:
     if not satellite.can_take():
