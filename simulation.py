cf2c256eedc296db10a231e4c636cdba69639842
mitryahina <mitryahina@ucu.edu.ua>
2018-12-03 13:11:16 +0200

    Update simulation.py
    added euristics

--- simulation.py
+++ simulation.py
@@ -10,7 +10,7 @@ class Satellite:
         self.max_w = max_w
         self.max_d = max_d
         self.turn = 0
-        self.d = [0, 0]
+        self.d = ([0, 0], [0, 0])
 
     def calc_position(self, turn):
         diff_turn = turn - self.turn
@@ -23,20 +23,25 @@ class Satellite:
                 self.lat = -648000 - self.lat
             self.vel = -self.vel
             self.lon = self.lon % 1296000 - 648000
+        self.d[0][0] = max(self.d[0][0] - self.max_w*diff_turn, -self.max_d)
+        self.d[0][1] = min(self.d[0][1] + self.max_w*diff_turn,  self.max_d)
+        self.d[1][0] = max(self.d[1][0] - self.max_w*diff_turn, -self.max_d)
+        self.d[1][1] = min(self.d[1][1] + self.max_w*diff_turn,  self.max_d)
         self.turn = turn
 
     def can_take(self, turn, location):
         self.calc_position(turn)
-        if self.lat + self.d[0] - min(self.max_w, self.max_d) < location[0] < self.lat + + self.d[0] + min(self.max_w, self.max_d)\
-                and self.lon + + self.d[1] - min(self.max_d, self.max_w) < location[1] < self.lon + self.d[1] + min(self.max_d, self.max_w):
-                return True
-        return False
+        if not ((self.lat + self.d[0][0] <= location[0] <= self.lat + self.d[0][1]) and
+                (self.lon + self.d[1][0] <= location[1] <= self.lon + self.d[1][1])):
+            return False
+        return True
 
     def take_photo(self, turn, location):
         if self.can_take(turn, location):
-            self.d[0] = location[0] - self.lat
-            self.d[1] = location[1] - self.lon
+            self.d[0][1] = self.d[0][0] = location[0] - self.lat
+            self.d[1][0] = self.d[1][1] = location[1] - self.lon
             return True
+        return False
 
     def __str__(self):
         return " ".join([str(i) for i in [self.lat, self.lon, self.vel, self.max_w, self.max_d]])
@@ -63,7 +68,10 @@ class Collection:
         return len(self.locations) == 0
 
     def get_rand_photo(self):
-        return self.locations[0]
+        return self.locations[randint(0, len(locations))]
+
+    def total_time(self):
+        return sum([i[1] - i[0] for i in self.ranges])
 
     __repr__ = __str__
 
@@ -77,26 +85,62 @@ class Simulation:
         self.current = 0
 
     def take_photo(self, col, ind, turn, sat):
-        if sat.can_take(turn, self.collections[col][ind]):
-            sat.take_photo(turn, self.collections[col][ind])
-            self.collections[col].pop(ind)
+        if sat.can_take(turn, self.collections[col].locations[ind]):
+            sat.take_photo(turn, self.collections[col].locations[ind])
+            self.collections[col].locations.pop(ind)
             if self.collections[col].is_empty():
                 self.score += collections[col].value
                 self.collections.pop(col)
 
     def simulate_full(self):
+        self.order_collection_value()
         while self.current < self.duration:
-            for col in range(len(self.collections)):
+            col = 0
+            while col < len(self.collections):
                 for sat in self.satellites:
                     if sat.can_take(self.current, self.collections[col].get_rand_photo()):
-                        print('Can take')
+                        print(self.current, "photo #", col)
                         self.take_photo(col, 0, self.current, sat)
+                        col -= 1
                         break
+                col += 1
+            self.current += 1
+        print(self.score)
+
+    def order_collection_value(self):
+        self.collections.sort(key=lambda x: x.value, reverse=True)
+
+    def order_collections_time(self):
+        self.collections.sort(key=lambda x: x.total_time())
+
+    def order_collections_qvalue(self):
+        self.collections.sort(key=lambda x: x.value / len(x.locations), reverse=True)
+
+    def order_colletions_tqvalue(self):
+        self.collections.sort(key=lambda x: x.value / (len(x.locations) * x.total_time()))
+
+    # TODO: подумати як би не проходитись по всіх колекціях
+    def find_place_suitable(self):
+        pass
+
+    def simulate_on_collections(self):
+        while self.current < self.duration:
+            self.order_collections_time()
+            col = 0
+            while col < len(self.collections):
+                if collections[col].time_suitable(self.current):
+                    for sat in self.satellites:
+                        if sat.can_take(self.current, self.collections[col].get_rand_photo()):
+                            print(self.current, "photo #", col)
+                            self.take_photo(col, 0, self.current, sat)
+                            col -= 1
+                            break
+                col += 1
             self.current += 1
         print(self.score)
 
 
-with open("final_round_2016.in/final_round_2016.in/forever_alone.in") as f:
+with open("final_round_2016.in/final_round_2016.in/constellation.in") as f:
     duration = f.readline()
     satellites = []
     for i in range(int(f.readline())):
@@ -106,7 +150,7 @@ with open("final_round_2016.in/final_round_2016.in/forever_alone.in") as f:
     collections = [None for i in range(num_collections)]
     for i in range(num_collections):
         collection_chr = f.readline().strip().split()
-        value = collection_chr[0]
+        value = int(collection_chr[0])
         num_locations = int(collection_chr[1])
         num_ranges = int(collection_chr[2])
         locations = [[int(i) for i in f.readline().strip().split()] for j in range(num_locations)]
@@ -115,7 +159,6 @@ with open("final_round_2016.in/final_round_2016.in/forever_alone.in") as f:
 
 
 s_alone = Simulation(duration, satellites, collections)
-s_alone.simulate_full()
 
 
-
+s_alone.simulate_on_collections()
